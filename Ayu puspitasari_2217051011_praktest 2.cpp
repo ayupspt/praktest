e2707b11df24e016e4fb45c1ca0e010fad7281a9
Ayupuspita <113082904+ayupspt@users.noreply.github.com>
2022-09-25 20:58:47 +0700

    Add files via upload

---
+++ Ayu puspitasari_2217051011_praktest 2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main () {
+	int input ;
+	cin >> input ;
+	if ( input == 1 ) {
+		cout << " satu " ; 
+	} else if ( input == 2 ) {
+		cout << " dua " ;
+	} else if ( input == 3 ) {
+		cout << " tiga " ;
+	} else if ( input >= 4 ) {
+		cout << " bilangan lebih besar dari tiga " ;
+		
+	}
+}
